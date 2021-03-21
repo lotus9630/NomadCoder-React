13c8781f6b84ed9b84af0343a89becd14e78448e
kim <lotus9630@gmail.com>
2021-03-21 17:49:20 +0900

    react-movie

---
+++ src/Movie.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Movie({ id, year, title, summary, poster, genres }) {
+  return (
+    <div className="movie">
+      <img src={poster} alt={title} title={title} />
+      <div className="movie__data">
+        <h3 className="movie__title">{title}</h3>
+        <h5 className="movie__year">{year}</h5>
+        <ul className="movie__genres">
+          {genres.map((genre, index) => {
+            return (
+              <li key={index} className="genres__genre">
+                {genre}
+              </li>
+            );
+          })}
+        </ul>
+        <p className="movie__summary">{summary.slice(0, 180)}...</p>
+      </div>
+    </div>
+  );
+}
+
+export default Movie;
